97a21bbb40303c3ec632ceba3170c1f7a4722003
Darina Derecha <darinaderechaa@gmail.com>
2023-01-17 18:34:03 +0200

    Add some math operations

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = -44;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println((double)a/b);
+        System.out.println(a * b);
+
+    }
 }
