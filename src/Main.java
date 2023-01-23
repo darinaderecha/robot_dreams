bc8e9806e1cc0a967cbb4f381802d293aa421f0a
Darina Derecha <darinaderechaa@gmail.com>
2023-01-23 14:30:06 +0200

    Add homework

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length() - 1));
+    }
 }
