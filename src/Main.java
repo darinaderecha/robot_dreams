d568b260be24bd8bba912d2d9a0146f9af3cd4f1
Darina Derecha <darinaderechaa@gmail.com>
2023-01-24 16:14:31 +0200

    Clean main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        int a = 2;
-        int b = -44;
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println((double)a/b);
-        System.out.println(a * b);
-
-    }
 }
