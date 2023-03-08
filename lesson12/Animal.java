38d18b591ef9435f319505af19ed8e2ff935359e
darinaderecha <darinaderecha@gmail.com>
2023-03-08 23:20:25 +0200

    Add lesson12

---
+++ lesson12/Animal.java
@@ -0,0 +1,5 @@
+public abstract class Animal extends Eukaryote {
+    public Animal() {
+        System.out.println("3. All animals belong to Animals domen");
+    }
+}
---
+++ lesson12/ChickenNelli.java
@@ -0,0 +1,10 @@
+public class ChickenNelli extends Chicken {
+    private String name = "Nelli";
+static {
+    System.out.println("0. From Chicken Nelli here is a small story about chickens");
+}
+    public ChickenNelli(String name) {
+        this.name = name;
+        System.out.println("6. Chickens are very intelligent animals.They have their own memories");
+    }
+}
